594907f17d462716e84273a0a7118f4c780ded45
FuBaooo <baihe2233@gmail.com>
2023-04-17 10:55:47 +0800

    feat: 回复限制

--- service/src/services/index.ts
+++ service/src/services/index.ts
@@ -1,10 +1,11 @@
 import type { Router } from 'express'
 import { authenticateToken } from 'src/middleware/auth'
+import { limiter } from 'src/middleware/limiter'
 import { chatConfig, chatProcess } from './chatgpt'
 import { authLogin, userUsage } from './user'
 
 export function setupRouters(router: Router) {
-  router.post('/chat-process', [authenticateToken], chatProcess)
+  router.post('/chat-process', [authenticateToken, limiter], chatProcess)
   router.get('/config', [authenticateToken], chatConfig)
 
   router.post('/login', authLogin)
